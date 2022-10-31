3f6cdc207440acb4157b5efe07539133fabd244b
sonree <115495546+sonree@users.noreply.github.com>
2022-10-31 17:20:09 +0300

    login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+//import 'dart:ui';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -8,13 +9,201 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _textController = TextEditingController();
+  final _passwordController = TextEditingController();
+  bool _isWhite = true;
+  bool _obscureText = true;
+
+  // Toggles the password show status
+  void _toggle() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
+  void _focus() {
+    setState(() {
+      _isWhite = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.greenAccent.shade400,
+      body: Stack(
+        children: [
+          Image(
+            image: const AssetImage('images/gym_2.jpg'),
+            fit: BoxFit.cover,
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+          ),
+          SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 12.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        icon: const Icon(
+                          Icons.arrow_back,
+                          color: Colors.white,
+                          size: 35.0,
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                      Text(
+                        'HYPEGYM',
+                        style: TextStyle(
+                          fontSize: 50.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.greenAccent[100],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.5,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: TextField(
+                    onTap: _focus,
+                    controller: _textController,
+                    style: const TextStyle(
+                      color: Colors.white,
+                    ),
+                    decoration: InputDecoration(
+                      isDense: true,
+                      filled: true,
+                      fillColor: Colors.black.withOpacity(0.8),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: Colors.greenAccent,
+                        ),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: Colors.greenAccent,
+                        ),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      labelText: 'Username',
+                      labelStyle: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 22,
+                      ),
+
+                      hintText: 'Username',
+                      hintStyle: TextStyle(
+                        fontSize: 20.0,
+                        color: _isWhite ? Colors.white : Colors.black,
+                      ),
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          _textController.clear();
+                        },
+                        icon: const Icon(Icons.clear),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: TextField(
+                    onTap: _focus,
+                    obscureText: _obscureText,
+                    controller: _passwordController,
+                    style: const TextStyle(
+                      color: Colors.white,
+                    ),
+                    decoration: InputDecoration(
+                      isDense: true,
+                      filled: true,
+                      fillColor: Colors.black.withOpacity(0.6),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: Colors.greenAccent,
+                        ),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                          color: Colors.greenAccent,
+                        ),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      labelText: 'Password',
+                      labelStyle: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 22,
+                      ),
+                      hintText: 'Password',
+                      hintStyle: TextStyle(
+                        fontSize: 20.0,
+                        color: _isWhite ? Colors.white : Colors.black,
+                      ),
+                      suffixIcon: IconButton(
+                        onPressed: _toggle,
+                        icon: Icon(
+                          _obscureText
+                              ? Icons.visibility_rounded
+                              : Icons.visibility_off_rounded,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  behavior: HitTestBehavior.opaque,
+                  onTap: () {
+                    debugPrint('Forgot Password?');
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          'Forgot Password?',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18.0,
+                            decoration: TextDecoration.underline,
+                            decorationStyle: TextDecorationStyle.dashed,
+                            decorationThickness: 2.0,
+                          ),
+                        ),
+                        ButtonTheme(
+                          minWidth: 150,
+                          child: MaterialButton(
+                            onPressed: () {},
+                            color: Colors.greenAccent[100],
+                            child: const Text(
+                              'Log In',
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
-      body: Column(),
     );
   }
 }
