bcde40bf74834206f1df9d1161947f500e1da58b
sonree <115495546+sonree@users.noreply.github.com>
2022-10-31 17:52:29 +0300

    Changed app theme

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'HypeGym',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: const WelcomePage(),
     );
