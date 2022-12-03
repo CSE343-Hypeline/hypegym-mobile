224257527667a270a8f5aa22153463ad2243e15a
Fatma Nur Sefer <72506424+fatmanursefeer@users.noreply.github.com>
2022-12-03 16:02:13 +0300

    update trainer profile page

--- lib/pages/admin_trainer_profile_page.dart
+++ lib/pages/admin_trainer_profile_page.dart
@@ -11,97 +11,161 @@ class _AdminTrainerProfilePageState extends State<AdminTrainerProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          color: Colors.grey.shade900,
-        ),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 10.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.arrow_back, color: Colors.white, size: 35.0,),
-                    onPressed: () => Navigator.of(context).pop(),
-                  ),
-                ],
-              ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.grey.shade900,
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 5.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10.0, bottom: 20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.arrow_back, color: Colors.white, size: 35.0,),
+                  onPressed: () => Navigator.of(context).pop(),
+                ),
+              ],
+            ),
+          ),
+          ListView(
+            physics: const BouncingScrollPhysics(),
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 40.0, bottom: 50.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "TRAINER PROFILE",
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.greenAccent.shade400,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 24),
+              Column(
                 children: [
                   Text(
-                    "TRAINERS",
+                    "Name",
                     style: TextStyle(
-                      fontSize: 50,
+                      fontSize: 20,
                       fontWeight: FontWeight.bold,
                       color: Colors.greenAccent.shade400,
                     ),
                   ),
+                  const SizedBox(height: 4),
+                  const Text(
+                    "Eray Ozkan",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+                  Text(
+                    "Email",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.greenAccent.shade400,
+                    ),
+                  ),
+                  const SizedBox(height: 4),
+                  const Text(
+                    "erayozkan@gmail.com",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+                  Text(
+                    "Phone",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.greenAccent.shade400,
+                    ),
+                  ),
+                  const SizedBox(height: 4),
+                  const Text(
+                    "+90 539 939 39 39",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+                  Text(
+                    "Experience",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.greenAccent.shade400,
+                    ),
+                  ),
+                  const Text(
+                    "3 Years",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+                  ElevatedButton(
+                    onPressed: (){
+                      //Navigator.push(context, MaterialPageRoute(builder: (context) => const AdminGymPage()));
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.greenAccent.shade700,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25)
+                      ),
+                      minimumSize: const Size(250, 30),
+                      maximumSize: const Size(250, 30),
+                    ),
+                    child: const Text(
+                      "List his/her trainees",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      textStyle: const TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      'Remove this trainer.',
+                      style: TextStyle(
+                        color: Colors.white,
+                        decoration: TextDecoration.underline,
+                        decorationStyle: TextDecorationStyle.dashed,
+                      ),
+                    ),
+                  ),
                 ],
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(5.0),
-              child: Text(
-                "Name",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.greenAccent.shade400,),
-              ),
-            ),
-            const Padding(
-                padding: EdgeInsets.all(5.0),
-                child: Text("Trainer")
-            ), //burda member adi olcak
-            Padding(
-              padding: const EdgeInsets.all(5.0),
-              child: Text(
-                "Email",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.greenAccent.shade400,),
-              ),
-            ),
-            const Padding(
-                padding: EdgeInsets.all(5.0),
-                child: Text("Trainer")
-            ), //burda member email olcak
-            Padding(
-              padding: const EdgeInsets.all(5.0),
-              child: Text(
-                "Phone",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.greenAccent.shade400,),
-              ),
-            ),
-            const Padding(
-                padding: EdgeInsets.all(5.0),
-                child: Text("Trainer")
-            ), //burda member telefon olcak
-            Padding(
-              padding: const EdgeInsets.all(5.0),
-              child: Text(
-                "Experience",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.greenAccent.shade400,),
-              ),
-            ),
-            const Padding(
-                padding: EdgeInsets.all(5.0),
-                child: Text("Trainer")
-            ), //burda member experience olcak
-            Padding(
-              padding: const EdgeInsets.all(5.0),
-              child: Text(
-                "Trainer",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.greenAccent.shade400,),
-              ),
-            ),
-            const Padding(
-                padding: EdgeInsets.all(5.0),
-                child: Text("Trainer")
-            ), //burda member trainer bilgisi olcak
-          ],
-        ),
+            ],
+          ),
+        ],
       ),
     );
   }
