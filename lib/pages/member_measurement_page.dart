7f04e7ac9e79a62ec98108b2d109ed3b3144921b
Fatma Nur Sefer <72506424+fatmanursefeer@users.noreply.github.com>
2022-12-11 16:24:14 +0300

    update member measurement page

--- lib/pages/member_measurement_page.dart
+++ lib/pages/member_measurement_page.dart
@@ -5,6 +5,114 @@ class MemberMeasurementPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.grey.shade900,
+            ),
+          ),
+          ListView(
+            children: [
+              DataTable(
+                columns: [
+                  DataColumn(
+                    label: Text(
+                      "Body Region",
+                      style: TextStyle(color: Colors.greenAccent.shade400, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  DataColumn(
+                    label: Text(
+                      "Last Month",
+                      style: TextStyle(color: Colors.greenAccent.shade400, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  DataColumn(
+                    label: Text(
+                      "Now",
+                      style: TextStyle(color: Colors.greenAccent.shade400, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+                rows: const [
+                  DataRow(
+                    cells: [
+                      DataCell(Text("Kilogram", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
+                      DataCell(Text("80", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
+                      DataCell(Text("85", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
+                    ]
+                  ),
+                  DataRow(
+                      cells: [
+                        DataCell(Text("Height", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
+                        DataCell(Text("170", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
+                        DataCell(Text("170", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
+                      ]
+                  ),
+                  DataRow(
+                      cells: [
+                        DataCell(Text("Wrist", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
+                        DataCell(Text("20", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
+                        DataCell(Text("20", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
+                      ]
+                  ),
+                  DataRow(
+                      cells: [
+                        DataCell(Text("Chest", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
+                        DataCell(Text("20", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
+                        DataCell(Text("20", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
+                      ]
+                  ),
+                  DataRow(
+                      cells: [
+                        DataCell(Text("Waist", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
+                        DataCell(Text("20", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
+                        DataCell(Text("20", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
+                      ]
+                  ),
+                  DataRow(
+                      cells: [
+                        DataCell(Text("Hip", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
+                        DataCell(Text("20", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
+                        DataCell(Text("20", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
+                      ]
+                  ),
+                  DataRow(
+                      cells: [
+                        DataCell(Text("Arm", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
+                        DataCell(Text("20", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
+                        DataCell(Text("20", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
+                      ]
+                  ),
+                  DataRow(
+                      cells: [
+                        DataCell(Text("Forearm", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
+                        DataCell(Text("20", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
+                        DataCell(Text("20", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
+                      ]
+                  ),
+                  DataRow(
+                      cells: [
+                        DataCell(Text("Leg", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
+                        DataCell(Text("20", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
+                        DataCell(Text("20", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
+                      ]
+                  ),
+                  DataRow(
+                      cells: [
+                        DataCell(Text("Calf", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
+                        DataCell(Text("20", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
+                        DataCell(Text("20", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),),
+                      ]
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
