8b47fdab121b915dffc36e1b96c8b988cf45edb1
erayozkan7 <eray.ozkan15@gmail.com>
2023-01-09 06:37:51 +0300

    App released

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -4,6 +4,6 @@ import 'dart:io' show Platform;
 class Constants {
   Constants._();
   //static const baseUrl = 'http://localhost:8080';
-  static String baseUrl =  Platform.isAndroid ? 'http://192.168.56.1:8080' : 'http://localhost:8080';
+  static String baseUrl =  "https://c159-78-190-131-60.eu.ngrok.io";
 //String baseUrl = Platform.isAndroid ? 'http://192.168.56.1:8080' : 'http://localhost:8080';
 }
